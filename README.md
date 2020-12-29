df65e4cff55759b208bb652a5731df2f6f9fb733
Gerd Stolpmann <gerd@figlyinc.com>
2020-12-29 19:03:21 +0100

    update

---
+++ README.md
@@ -0,0 +1,82 @@
+# animated-card web component for RemixLabs
+
+## Register
+
+```
+TAG:
+animated-card
+
+MODTYPE:
+url
+
+DATA:
+https://gerdstolpmann.github.io/animatedcard/index.js
+
+INS:
+[ { "name":"keyframes", "type":[{}] },
+  { "name":"options", "type":{} },
+  { "name":"seqNumber", "type": "number" },
+  { "name":"addToQueue", "type":"event" },
+  { "name":"pause", "type":"event" },
+  { "name":"cancel", "type":"event" },
+  { "name":"play", "type":"event" },
+  { "name":"finish", "type":"event" }
+]
+
+EVENTS:
+[]
+
+FIELD LABEL TEXT:
+1 Card
+```
+
+## Animate a card
+
+This component is an interface for the
+[https://developer.mozilla.org/en-US/docs/Web/API/Element/animate](animate)
+function of HTML elements. You can pass `keyframes` and `options`
+as described in the standard, e.g.
+
+```
+keyframes:
+  [ { transform: "translateX(0px)" },
+    { transform: "translateX(100px)" }
+  ]
+
+options:
+  { duration: 1000,
+    fill: "forwards"
+  }
+```
+
+This animation is then applied to the card that is passed in.
+
+
+## Queuing up animations
+
+While the current animation is playing, it is possible to submit further
+animations for the same card by triggering the `addToQueue` event.
+These additional animations queue up, and when the first animation
+finishes (either naturally or because of a `finish` event), the next
+animation in the queue is automatically and seamlessly started.
+
+There is no upper bound for the number of queue elements. However, it
+is usually not wise to submit more than one waiting animation.
+
+
+## Controlling the start and finish time exactly
+
+The `options` object interprets two non-standard fields: `options.startAt`
+specifies the time when the animation will start playing. `startAt` is
+given in milliseconds since the time the web component was instantiated.
+
+The other field is `options.finishAt`. If present it overrides the
+`options.duration` field - in other words, the `duration` is derived
+from `finishAt`.
+
+## Sequence numbers
+
+If a `seqNumber` input is passed in, the behavior of the `addToQueue`
+event changes slightly. The new values are only added to the queue
+when the `seqNumber` has been increased since the last time an
+animation was added.
--- index.js
+++ index.js
@@ -1,39 +1,13 @@
 "use-strict";
 
-/*
-Register this web component as:
-
-TAG:
-animated-card
-
-MODTYPE:
-url
-
-DATA:
-https://gerdstolpmann.github.io/animatedcard/index.js
-
-INS:
-[ { "name":"keyframes", "type":[{}] },
-  { "name":"options", "type":{} },
-  { "name":"addToQueue", "type":"event" },
-  { "name":"pause", "type":"event" },
-  { "name":"cancel", "type":"event" },
-  { "name":"play", "type":"event" },
-  { "name":"finish", "type":"event" }
-]
-
-EVENTS:
-[]
-
-*/
-
-
 class AnimatedCard extends HTMLElement {
     constructor() {
         super();
 
         this.current = null;
+        this.seqNumber = null;
         this.queue = [];
+        this.zeroTime = Date.now();
 
         this.attachShadow({mode: 'open'});
         let wrapper = document.createElement('div');
@@ -45,11 +19,18 @@ class AnimatedCard extends HTMLElement {
     }
 
     static get observedAttributes() {
-        return ["keyframes", "options"];
+        return ["keyframes", "options", "seqNumber"];
     }
 
     addToQueue() {
         console.log("animated-card: addToQueue");
+        let attr_seqNumber = this.getAttribute("seqNumber");
+        if (attr_seqNumber) {
+            let seqNumber = parseInt(attr_seqNumber, 10);
+            if (this.seqNumber !== null && this.seqNumber >= seqNumber)
+                return;  // ignore update
+            this.seqNumber = seqNumber;
+        };
         let new_keyframes = [];
         let new_options = {};
         let attr_keyframes = this.getAttribute("keyframes");
@@ -100,12 +81,26 @@ class AnimatedCard extends HTMLElement {
 
     nextAnimation() {
         console.log("animated-card: nextAnimation");
+        if (this.queue.length == 0)
+            return;
         let next = this.queue.shift();
-        if (next) {
-            let elemThis = this;
+        let time = Date.now() - this.zeroTime;
+        let waitTime = 0;
+        if (next.options && next.options.startAt !== undefined && next.options.startAt >= time)
+            waitTime = next.options.startAt - time;
+        let elemThis = this;
+        window.setTimeout(function() {
+            if (next.options && next.options.finishAt !== undefined) {
+                let time = Date.now() - elemThis.zeroTime;
+                let dur = next.options.finishAt - time;
+                if (dur < 0) dur = 0;
+                next.options.duration = dur;
+            };
+            delete(next.options.startAt);
+            delete(next.options.finishAt);
             elemThis.current = elemThis.shadowTop.animate(next.keyframes, next.options);
             elemThis.current.onfinish = function() { elemThis.onAnimFinish() };
-        }
+        }, waitTime);
     }
 
     onAnimFinish() {
